72048238dbf7dbfcbdd36a6c5b4f61a77da03722
Elad-lab <elad7m7@gmail.com>
2023-07-04 18:46:05 +0300

    initial commit

---
+++ 1.1_primitive_data_types/1.1_primitive_data_types.js
@@ -0,0 +1,16 @@
+let message = `You enter a dimly lit room.`;
+let treasureCount = 100
+let key = {object : "goldenKey", carvings: "intricate"};
+let isTrapActivated =false
+let secretPassage  = null
+let mysteriousArtifact = undefined
+let ancientSymbol = {symbol : `ancient symbol etched onto a stone tablet`}
+
+
+console.log(typeof message);
+console.log(typeof treasureCount);
+console.log(typeof key);
+console.log(typeof isTrapActivated);
+console.log( typeof secretPassage);
+console.log(typeof mysteriousArtifact);
+console.log(typeof ancientSymbol );
